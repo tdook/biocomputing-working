1865ddf04dc9145b22bf16a806007fc9f9383a3b
TobyDook <c2020110@ncl.ac.uk>
2024-03-12 14:13:43 +0000

    many things added, graph plotting, sigma decay,roulette selection, std dev update, shape adding

--- evoart.py
+++ evoart.py
@@ -4,7 +4,14 @@ Genetic algorithm implemented with Evol solving the one max problem
 (maximising number of 1s in a binary string).
 
 """
+import math
 import random
+
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('TkAgg')
+import numpy as np
+
 
 import evol
 from PIL import ImageChops
@@ -14,6 +21,9 @@ from evol import Population, Evolution
 
 SIDES = 3
 POLYGON_COUNT = 100
+INIT_SIGMA = 15
+DECAY_RATE = 0.05
+CURRENT_GEN = 0
 
 MAX = 255 * 200 * 200
 TARGET = Image.open("6a.png")
@@ -34,17 +44,9 @@ def make_polygon():
   b = random.randrange(0,256)
   a = random.randrange(30,60)
 
-  x1 = random.randrange(11, 189)
-  y1 = random.randrange(11, 189)
-  x2 = random.randrange(11, 189)
-  y2 = random.randrange(11, 189)
-  x3 = random.randrange(11, 189)
-  y3 = random.randrange(11, 189)
-  x4 = random.randrange(11, 189)
-  y4 = random.randrange(11, 189)
 
   coords = [(random.randrange(11,189),random.randrange(11,189)) for _ in range (SIDES)]
-  #print (coords)
+
 
   return[(r,g,b,a)]+ coords
   #return[(r,g,b,a),(x1,y1), (x2,y2), (x3,y3),(x4,y4) ]
@@ -60,29 +62,25 @@ def initialise():
 def evolve(population, args, cxp=0.5, mutp=0.5):
     population.survive(fraction=0.5)
     population.breed(parent_picker=select, combiner=combine)
-    population.mutate(mutate_function=mutate, rate=0.3)
-    #population.individuals = [replace(ind, population, cxp,mutp) for ind in population.individuals]
-   # print("evolved")
+    population.mutate(mutate_function=mutate, rate=0.5)
+
     return population
-    #exit()
 
-
-# def replace(individual, population, cxp=0.5, mutp=0.5, ):
-#      print("replaced----------------")
-#      p = random.random()
-#      if p < cxp:
-#          offspring = evol.utils.offspring_generator(
-#              population.individuals, parent_picker=select, combiner=combine)
-#          return next(offspring)
-#      if cxp < p < cxp + mutp:
-#          return individual.mutate(mutate_function=mutate, probability=0.1)
-#      if cxp + mutp < p:
-#          return individual
+def roulette_selection(population):
+    total_fitness = sum(individual.fitness for individual in population)
+    selection_point = random.uniform(0, total_fitness)
+    cumulative_fitness = 0
+    for individual in population:
+        cumulative_fitness += individual.fitness
+        if cumulative_fitness >= selection_point:
+            return individual
 
 
 def select(population):
   #print("selected")
   return [random.choice(population) for i in range(2)]
+  #return [roulette_selection(population) for _ in range(2)]
+
 
 
 def combine(*parents):
@@ -91,19 +89,31 @@ def combine(*parents):
 
 
 
-
+def updated_std_dev(init_std_dev, generation, total_generations):
+    return init_std_dev * (1 - generation / total_generations)
 
 def mutate(solution, rate):
-  #print("mutating")
+  global INIT_SIGMA, DECAY_RATE, CURRENT_GEN
   solution = list(solution)
+
+  current_sigma = INIT_SIGMA / math.log(CURRENT_GEN + 2, 2)
+
+  #len solution < max poly count and random.rand < p:
+  if random.random() < 0.3:
+      solution.append(make_polygon())
+      return solution
+  #generate new polys
 
   if random.random() < 0.5:
     # mutate points
     i = random.randrange(len(solution))
     polygon = list(solution[i])
     coords = [x for point in polygon[1:] for x in point]
-    coords = [x if random.random() > rate else
-              x + random.normalvariate(0, 10) for x in coords]
+    coords = [x + random.uniform(-1, 1) if random.random() > rate else x for x in coords]
+    #coords = [x if random.random() > rate else
+            #  x + random.normalvariate(0,current_sigma) for x in coords]
+
+
     coords = [max(0, min(int(x), 200)) for x in coords]
     polygon[1:] = list(zip(coords[::2], coords[1::2]))
     solution[i] = polygon
@@ -113,10 +123,13 @@ def mutate(solution, rate):
       polygon = list(solution[i])
       colors = [x for color in polygon[1:] for x in color]
       colors = [x if random.random() > rate else
-                x + random.normalvariate(0,10) for x in colors]
+                x + random.normalvariate(0,current_sigma) for x in colors]
       colors = [max(0, min(int(x), 255)) for x in colors]
       polygon[1:] = list(zip(colors[::2], colors[1::2]))
       solution[i] = polygon
+
+
+  #polygon = list(solution[i])
 
 
 
@@ -130,36 +143,31 @@ def mutate(solution, rate):
 
   return solution
 
-
 def draw(solution):
   image = Image.new("RGB", (200, 200))
   canvas = ImageDraw.Draw(image, "RGBA")
   for polygon in solution:
     canvas.polygon(polygon[1:], fill=polygon[0])
+
+
+
   return image
 
 
+# Generate values for generations
+generations = np.arange(1, 100)
+sigma_values = INIT_SIGMA / np.log(generations + 2)
+
+def plot_graph(generations, sigma_values):
+
+    plt.plot(generations, sigma_values)
+    plt.xlabel('Generation')
+    plt.ylabel('Current Sigma')
+    plt.title('Decay of Sigma over Generations')
+    plt.grid(True)
+    plt.show()
 
 
 
 
-#population = Population.generate(initialise, evaluate, size=10, maximize=True)
-#population.mutate(mutate_function=mutate, rate=0.1)
-
-#evolution1 = (Evolution().survive(fraction=0.5)
-          ##   .breed(parent_picker=select, combiner=combine)
-         #    .mutate(mutate_function=mutate, rate=0.1)
-         #    .evaluate())
-#population.evolve(population,n=1)
-#population.survive(fraction=0.5).breed(parent_picker=select, combiner=combine)
-#population.evolve(evolution1,n=1)
-
-'''mew = size of population, lamda is size of offspring
-'''
-#population.breed()
-#population.map(replace, parents=population.individuals, cxp=0.7, mutp=0.2)
-#population.survive(.mutate(mutate_function=mutate(),probability=0.1),)
-
-#draw(population[0].chromosome).save("solution.png")
-#draw(evolution[0].chromosome).save("solution.png")
-
+#plot_graph(generations,sigma_values)
